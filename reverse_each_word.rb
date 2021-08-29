29cb5e7e49f89ca1ae5ba401c92b8b8f1540f223
Coy Reid <coyreid7@gmail.com>
2021-08-29 16:03:55 -0500

    commit tree c8fd3724bf06eab8e60278cf4bdd1947e9490bb1

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,1 +1,45 @@
-# Write your code here
+require 'pry'
+
+def reverse_each_word (sentence)
+  words = sentence.split
+  reversed_words = []
+
+  words.each do |word|
+    reversed_words << word.reverse
+  end
+
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word("Hello there, and how are you?")
+
+#another way of doing it
+def reverse_each_word_map (sentence)
+  words = sentence.split
+
+  reversed_words = words.map do |word|
+    word.reverse
+  end
+
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word_map("Hello there, general kenobi")
+
+#or even shorter
+def reverse_each_word_map_two (sentence)
+  words = sentence.split
+
+  reversed_words = words.map(&:reverse)
+
+  reversed_words.join(" ")
+end
+
+puts reverse_each_word_map_two("Hello there, general kenobi")
+
+#or even in one line
+def reverse_each_word_map_three (sentence)
+  sentence.split.map(&:reverse).join(" ")
+end
+
+puts reverse_each_word_map_three("Hello there, general kenobi")
